f6f64908c6f35f89744b24412c0ea6b8afec19d1
beztao01 <fernandosandoval@beztao.com>
2023-05-06 08:01:51 -0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+
+$(function(){
+    $(".twentytwenty-container[data-orientation!='vertical']").twentytwenty({default_offset_pct: 0.5});
+    $(".twentytwenty-container[data-orientation='vertical']").twentytwenty({default_offset_pct: 0.3, orientation: 'vertical'});
+  });
+  
